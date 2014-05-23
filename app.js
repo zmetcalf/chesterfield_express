7d0bd92f81865eb514fd8c45ab2bc1c348600b5d
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-05-23 13:56:02 -0600

    Starting app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+  logger = require('morgan'),
+  swig = require('swig'),
+  path = require('path'),
+  _ = require('underscore');
+
+var app = module.exports = express();
+
+// Setup Swig templates
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'app/views'));
+
+app.set('view cache', false);
+
+// Change to true for production
+swig.setDefaults({ cache: false });
+
+// Logger
+if (!module.parent) app.use(logger('dev'));
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
