07c8184802b874252d5cb78d0ada5107d7810026
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-06-04 16:06:40 -0600

    Adding forms to user

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,4 +1,10 @@
-var model = require('./models/models');
+var model = require('./models/models'),
+    forms = require('forms'),
+    fields = forms.fields,
+    validators = forms.validators,
+    widgets = forms.widgets,
+    bootstrap_field = require('../../lib/forms').bootstrap_field,
+    _ = require('underscore');
 
 exports.before = function(req, res, next) {
   if (req.session.user && req.session.user.is_staff) {
@@ -29,6 +35,7 @@ exports.list = function(req, res, next) {
 
 exports.edit = function(req, res, next) {
   res.render('edit', {
+    form: update_form(req.user).toHTML(bootstrap_field),
     user: req.user,
     csrf_token: req.csrfToken()
   });
@@ -39,6 +46,82 @@ exports.show = function(req, res, next) {
 }
 
 exports.update = function(req, res, next) {
-  res.message('Information updated!');
-  res.redirect('/user/' + req.user.username);
+  var update_fields = {};
+  _.each(['first_name', 'last_name', 'is_staff'], function(field) {
+    if(req.body[field]) {
+      update_fields[field] = req.body[field];
+    }
+  });
+
+  model.User.findOneAndUpdate({ username: req.user.username }, update_fields,
+    function(err, user) {
+      if (err) return console.log(err);
+      res.message('Information updated!');
+      res.redirect('/user/' + req.user.username);
+  });
 }
+
+exports.create function(req, res, next) {
+
+}
+
+exports.add = function(req, res, next) {
+
+}
+
+function update_form(user) {
+  return forms.create({
+    first_name: fields.string({
+      widget: widgets.text(),
+      errorAfterField: true,
+    }),
+
+    last_name: fields.string({
+      widget: widgets.text(),
+      errorAfterField: true,
+    }),
+
+    is_staff: fields.boolean({
+      widget: widgets.select(),
+      choices: {
+        true: "Yes",
+        false: "No"
+      },
+
+      errorAfterField: true,
+
+      value: user.is_staff
+    })
+  });
+}
+
+var create_form = forms.create({
+  first_name: fields.string({
+    widget: widgets.text(),
+    required: true,
+    errorAfterField: true,
+  }),
+
+  last_name: fields.string({
+    widget: widgets.text(),
+    required: true,
+    errorAfterField: true,
+  }),
+
+  username: fields.string({
+    widget: widgets.text(),
+    required: true,
+    errorAfterField: true,
+  }),
+
+  is_staff: fields.boolean({
+    widget: widgets.select(),
+    choices: {
+      true: "Yes",
+      false: "No"
+    },
+    required: true,
+    errorAfterField: true,
+    value: false
+  })
+});
