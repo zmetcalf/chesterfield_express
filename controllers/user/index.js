bb88b090bd7733a8b80f9102920e906f825b0cfb
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-06-05 14:25:31 -0600

    Working on creating users

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,10 +1,12 @@
 var model = require('./models/models'),
+    crypto = require('crypto'),
+    hash = require('pwd').hash,
+    _ = require('underscore'),
     forms = require('forms'),
     fields = forms.fields,
     validators = forms.validators,
     widgets = forms.widgets,
-    bootstrap_field = require('../../lib/forms').bootstrap_field,
-    _ = require('underscore');
+    bootstrap_field = require('../../lib/forms').bootstrap_field;
 
 exports.before = function(req, res, next) {
   if (req.session.user && req.session.user.is_staff) {
@@ -61,12 +63,51 @@ exports.update = function(req, res, next) {
   });
 }
 
-exports.create function(req, res, next) {
-
+exports.create = function(req, res, next) {
+  res.render('create', {
+    form: create_form.toHTML(bootstrap_field),
+    csrf_token: req.csrfToken()
+  });
 }
 
 exports.add = function(req, res, next) {
+  create_form.handle(req, {
+    success: function(form) {
+      gen_password(function(err, password) {
+        if (err) return console.log(err);
+        hash(password, function(err, _salt, _hash) {
+          if (err) return console.log(err);
 
+          model.User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            is_staff: req.body.is_staff,
+            hash: _hash,
+            salt: _salt
+          }, function(err, new_user) {
+            if(err) return handleError(err);
+            res.message('New Password is: ' + password);
+            res.redirect('/user/' + req.body.username);
+          });
+        });
+      });
+    },
+
+    error: function(form) {
+      res.render('create', {
+        form: create_form.toHTML(bootstrap_field),
+        csrf_token: req.csrfToken()
+      });
+    },
+
+    empty: function(form) {
+      res.render('create', {
+        form: create_form.toHTML(bootstrap_field),
+        csrf_token: req.csrfToken()
+      });
+    }
+  });
 }
 
 function update_form(user) {
@@ -112,6 +153,7 @@ var create_form = forms.create({
     widget: widgets.text(),
     required: true,
     errorAfterField: true,
+    validators: [ unique_user ],
   }),
 
   is_staff: fields.boolean({
@@ -122,6 +164,29 @@ var create_form = forms.create({
     },
     required: true,
     errorAfterField: true,
-    value: false
+    value: "false"
   })
 });
+
+function unique_user(form, field, callback) {
+  model.User.find({}, 'username', function(err, users) {
+    if (err) return console.log(err);
+
+    if(!!_.find(users, function(user) { return field.data == user; })) {
+      return callback('Username already taken.');
+    } else {
+      return callback();
+    }
+  });
+}
+
+function gen_password(callback) {
+  crypto.randomBytes(10, function(err, buf) {
+    if(err) return console.log(err);
+    buf.toString('base64')
+    .slice(0, 10)
+    .replace(/\+/g, '0')
+    .replace(/\//g, '0');
+    return callback(buf);
+  });
+}
