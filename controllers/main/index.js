2508a3cd7d3d0a23c915bf578fb275ce92bb9765
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-06-12 12:21:00 -0600

    Updated theme

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,6 +1,6 @@
 exports.index = function(req, res) {
   var page_content = {
-    "content_heading": "Welcome to Chesterfield Beans!",
+    "content_heading": "Welcome to Chesterfield Express!",
     "content": "Here is the place for some things node.",
     "call_to_action" : {
       "link": "#",
