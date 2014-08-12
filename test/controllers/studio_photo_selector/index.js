a1cfc9f2cd8b755c58380245a1f5a99736d685de
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-08-12 12:35:46 -0600

    Test for getting photos assigned to studio completed

--- test/controllers/studio_photo_selector/index.js
+++ test/controllers/studio_photo_selector/index.js
@@ -1,8 +1,12 @@
 var assert = require('chai').assert,
     async = require('async'),
+    hash = require('pwd').hash,
+    cheerio = require('cheerio'),
     request = require('supertest'),
     mongoose = require('mongoose'),
+    db_opts = require('../../../config/db');
     user_model = require('../../../controllers/user/models/models'),
+    photo_model = require('../../../controllers/photo/models/models'),
     model = require('../../../controllers/studio/models/models');
 
 if(!mongoose.connection.readyState) {
@@ -14,37 +18,135 @@ var app = require('../../../index');
 var server = request.agent(app);
 var csrf = '';
 
-describe.skip('Studio Photo Selector', function() {
+describe('Studio Photo Selector', function() {
+  var photo_one = null;
+  var photo_two = null;
+
   beforeEach(function(done) {
-    user_model.User.create({
-      first_name: 'Test',
-      last_name: 'User',
-      username: 'auth_user',
-      is_staff: true,
-      hash: 'fpIO7HOtq3FsYAOW9giPQAqfm0QXhB1tR7RDv9RVrch2wKURPlUJUg4YZmfISdXW1nzUP+P1ZXA6xIxQLrHldAC2WVfj7HrAOeERleGmIRxc4Jb0hCE9ObKLQa9rQZKfGJf9EhW+vunWAujXdt+bfAqj2px7oIhqFdPCN1guIhg=',
-      salt: 'WlSDOruUy6E+o3KPr/QbzHaAGa4+25+07TzIyhZj5tkANxal0Slx47CpLZo9FbvH59lys4SOqyDQwjbtbVstMqzcLkcoaMD/2tr51CIlkjsy5Wr38W7aNx8PXPT/JcV31cZBBBs95MeOBXeJc6FKuuqTU55U3p0gOp4MzUKeReg='
-    }, function(err, user) {
-      if(err) return console.log(err);
-      model.Content.create({
-        title: 'Title',
-        summary: 'Summary',
-        content: 'Content',
-        published: true,
-        _author: user._id,
-        seo_keywords: 'seo_key',
-        seo_description: 'seo_descript',
-        url_slug: 'blog'
-      }, function(err, content) {
-        if(err) return console.log(err);
-        done();
-      });
-    });
+    var user = null;
+    var local_hash = '';
+    var local_salt = '';
+
+    async.series([
+      function(callback) {
+        hash('foobar', function(err, _salt, _hash) {
+          if(err) return console.log(err);
+          local_hash = _hash;
+          local_salt = _salt;
+          callback(null);
+        });
+      },
+
+      function(callback) {
+        user_model.User.create({
+          first_name: 'Test',
+          last_name: 'User',
+          username: 'auth_user',
+          is_staff: true,
+          hash: local_hash,
+          salt: local_salt
+        }, function(err, new_user) {
+          if(err) return console.log(err);
+          user = new_user;
+          callback(null, user);
+        });
+      },
+
+      function(callback) {
+        photo_model.Photo.create({
+          title: 'Photo One',
+          description: 'Desc1',
+          published: true,
+          path: 'asdf',
+          _author: user
+        }, function(err, photo) {
+          if(err) return console.log(err);
+          photo_one = photo;
+          callback(null, photo);
+        });
+      },
+
+      function(callback) {
+        photo_model.Photo.create({
+          title: 'Photo Two',
+          description: 'Desc2',
+          published: true,
+          path: 'qwer',
+          _author: user
+        }, function(err, photo) {
+          if(err) return console.log(err);
+          photo_two = photo;
+          callback(null, photo);
+        });
+      },
+
+      function(callback) {
+        model.Studio.create({
+          title: 'Title',
+          description: 'Description',
+          published: true,
+          _author: user._id,
+          seo_keywords: 'seo_key',
+          seo_description: 'seo_descript',
+          url_slug: 'studio',
+          _photos: [ photo_one, photo_two ]
+        }, function(err, content) {
+          if(err) return console.log(err);
+          callback(null, content);
+        });
+      },
+
+      function(callback) {
+        server
+          .get('/login')
+          .end(function(err, res) {
+            if(err) console.log(err);
+            var $ = cheerio.load(res.text);
+            csrf = $('input[name="_csrf"]').val();
+            callback(null);
+        });
+      },
+
+      function(callback) {
+        server
+          .post('/login')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+          .send({ username: 'auth_user', password: 'foobar', _csrf: csrf })
+          .end(function(err, res) {
+            if(err) console.log(err);
+            callback(null);
+          });
+      },
+    ], function(err, results) { done(); });
   });
 
-  afterEach(function() {
-    var q = model.Content.remove({});
-    q.exec();
-    q = user_model.User.remove({});
-    q.exec();
+  afterEach(function(done) {
+    async.series([
+      function(callback) {
+        model.Studio.remove({}).exec(callback);
+      },
+      function(callback) {
+        user_model.User.remove({}).exec(callback);
+      },
+      function(callback) {
+        server
+        .post('/logout')
+        .end(function(err, res) {
+          if(err) console.log(err);
+          callback(null);
+        });
+      }
+    ], function (err, results) { done(); });
+  });
+
+  it('should return assigned photos', function(done) {
+    model.Studio.findOne({ url_slug: 'studio' }, function(err, studio) {
+      if (err) return console.log(err);
+      server
+        .get('/studio_photo_selector/' + studio._id.toString())
+        .expect('[\n  "' + photo_one._id.toString() + '",\n  "' +
+                photo_two._id.toString() + '"\n]')
+        .expect(200, done);
+    });
   });
 });
