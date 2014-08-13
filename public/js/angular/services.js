ec8e04d464ff58160f8764ca7c2782b3644e0764
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-08-13 17:04:16 -0600

    Added underscore to dashboard

--- public/js/angular/services.js
+++ public/js/angular/services.js
@@ -1,19 +1,27 @@
 var studio_services = angular.module('studio_services', []);
 
-studio_services.factory('photos', [ '$http', function($http) {
-  var get_photos = function() {
-    return $http.get('/studio_photo_selector/' +
-      angular.element('#id_id').val()).success(function(data) {
-        alert(data.all_photos);
-      });
-  }
+studio_services.factory('photos', [ '$http', '$filter',
+  function($http, $filter) {
+    var get_photos = function() {
+      return $http.get('/studio_photo_selector/' +
+        angular.element('#id_id').val()).success(function(data) {
+          var selected_photos = _.filter(data.all_photos, function(photo) {
+            _.each(data.studio_photos, function(sp) {
+              if (angular.equals(photo._id, sp)) {
+                return true;
+              }
+              return false;
+            });
+          });
+        });
+    }
 
-  var update_photos = function() {
-    alert('UPDATE PHOTOS');
-  }
+    var update_photos = function() {
+      alert('UPDATE PHOTOS');
+    }
 
-  return {
-    get_photos: get_photos,
-    update_photos: update_photos
-  }
+    return {
+      get_photos: get_photos,
+      update_photos: update_photos
+    }
 }]);
